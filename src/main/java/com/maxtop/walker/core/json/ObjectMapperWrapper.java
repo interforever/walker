8ae4fb505447335e3d8483253cfb10e19e8f1499
slli <slli@usky.com.cn>
2017-08-31 11:38:54 +0800

    commit

--- src/main/java/com/maxtop/walker/core/json/ObjectMapperWrapper.java
+++ src/main/java/com/maxtop/walker/core/json/ObjectMapperWrapper.java
@@ -1,13 +1,18 @@
+
 package com.maxtop.walker.core.json;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
 public class ObjectMapperWrapper extends ObjectMapper {
 	
 	private static final long serialVersionUID = -7755932610450962546L;
-
+	
 	public ObjectMapperWrapper() {
 		disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+		this.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+		this.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 	}
+	
 }
